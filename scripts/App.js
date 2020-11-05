2f942aa364986e6cc9e976196068278163110f7d
Kris Hollenbeck <kris.hollenbeck@infor.com>
2020-11-04 21:58:52 -0600

    second

--- scripts/App.js
+++ scripts/App.js
@@ -1,122 +1,173 @@
 const fsExtra = require('fs-extra');
-const Axios = require('axios');
-const VueAxios = require('vue-axios');
+const https = require('https');
 
-const app = Vue.createApp({})
+const k = '6083FC2E5FF4E12C32107A965731CC4C';
+const createState = (state) => {
+    return new Proxy(state, {
+            set(target, property, value) {
+            target[property] = value;
+            render();
+            return true;
+        }
+    });
+};
 
-app.use(VueAxios, Axios)
+const state = createState({
+    steamapplocation: '',
+    moddata: ''
+});
 
-app.component('form-component', {
-  data() {
-    return {
-      highlight: false,
-      steamAppsPath: "E:\\Steam\\steamapps", // "path/to/your/steamapps",
-      gameNames: []
-    }
-  },
-  renderTracked({ key, target, type }) {
-    console.log({ key, target, type })
-    /* This will be logged when component is rendered for the first time:
-    {
-      key: "cart",
-      target: {
-        cart: 0
-      },
-      type: "get"
-    }
-    */
-  },
-  updated() {
-    this.$nextTick(function () {
-      // Code that will run only after the
-      // entire view has been re-rendered
-    })
-  },
-  methods: {
-    generate: (path) => {
-      debugger
-      if (path === "path/to/your/steamapps") {
+const listeners = document.querySelectorAll('[data-model]');
+const generateButton = document.getElementById('generate');
+
+listeners.forEach((listener) => {
+    const steamapplocation = listener.dataset.model;
+
+    listener.addEventListener('keyup', (event) => {
+        state[steamapplocation] = listener.value;
+    });
+});
+
+generateButton.addEventListener('click', (event) => {
+    generate();
+});
+
+const render = () => {
+    const bindings = Array.from(document.querySelectorAll('[data-binding]')).map(
+        e => e.dataset.binding
+    );
+
+    bindings.forEach((binding) => {
+        document.querySelector(`[data-model='${binding}']`).innerHTML = state[binding];
+        document.querySelector(`[data-model='${binding}']`).value = state[binding];
+    });
+};
+
+async function generate() {
+    debugger
+    
+    const path = state.steamapplocation;
+
+    if (path === "path/to/your/steamapps") {
         alert('Path is invalid! Please enter a valid path.');
         return;
-      }
+    }
 
-      if (path.split('\\').length > 1 
-        && path.split('\\').includes('steamapps')
-        && path.split('\\')[path.split('\\').length -1] === 'steamapps'
-      ) {
+    if (path.split('\\').length > 1 
+    && path.split('\\').includes('steamapps')
+    && path.split('\\')[path.split('\\').length -1] === 'steamapps'
+    ) {
         const p = `${path}\\workshop`;
         const workshopFiles = [];
 
         fsExtra.readdirSync(p).forEach(file => {
-          if (file.split('.').includes('acf')) {
-            workshopFiles.push(file);
-          }
+            if (file.split('.').includes('acf')) {
+                workshopFiles.push(file);
+            }
         });
 
         if (workshopFiles.length === 0) {
-          alert('Cannot find any workshop files');
-          return;
+            alert('Cannot find any workshop files');
+            return;
         }
 
-        workshopFiles.forEach((f) => {
-          const filePath = `${path}\\workshop\\${f}`;
-          fsExtra.readFile(filePath, "utf8", (err, data) => {
-            if (err) {
-              alert(err);
-            }
-            
-            const steamAppId = getSteamAppId(data);
-            const apiUrl = `http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=6083FC2E5FF4E12C32107A965731CC4C&appid=${steamAppId}`;
-            
+        const modData = await getModData(path, workshopFiles);
+        console.log(modData)
 
-            // const d = app.$http.get(apiUrl);
-
-            Axios.get(apiUrl).then((response) => {
-              if (response.data.game && response.data.game.gameName) {
-                app.component('form-component').data().gameNames.push(response.data.game.gameName);
-                // Vue.nextTick(() => {
-                //   app.component('form-component').data().gameNames.push(response.data.game.gameName);
-                //   app.component('form-component').reRender();
-                // })
-              }
-            });
-          });
-        });
-      } else {
+        buildResultsTable(modData);
+    } else {
         alert('Path is invalid! Please enter a valid path.');
-      }
-
-      function getSteamAppId(data) {
-        const trimed = data.split('"').map((s) => s.trim());
-        
-        // Remove empty quotes
-        const trimed2 = trimed.filter((s) => s.length !== 0);
-        const appId = trimed2[trimed2.findIndex((s, i) => s === 'appid') + 1];
-
-        return appId;
-      }
     }
-  },
-  template: `
-    <div class="flex-header">
-      <label class="label">Steam apps location:</label>
-      <a href="javascript:void(0)" title="Copy your steam apps path and paste it into the steam apps location input.">
-        <img @mouseenter='highlight = !highlight' @mouseleave='highlight = !highlight' class="info-icon" src="./images/info.png" alt="" />
-      </a>
-    </div>
-    
-    <div class="input-btn-wrapper">
-      <input v-model="steamAppsPath" :class="{highlight: highlight}" type="text" />
-      <button @click="generate(steamAppsPath)">GENERATE</button>
-    </div>
-    
-    <label class="label">Output:</label>
-    <table id="output" class="output">
-      <tr :key="renderKey" v-for="gameName in gameNames">
-        <td>{{gameName}}</td>
-      </tr>
-    </table>
-  `  
-})
+}
 
-app.mount('#app');
+function buildResultsTable(modData) {
+    let html = modData.length > 0 ? [] : 'NO DATA WAS FOUND';
+
+    modData.forEach((d) => {
+        let temp = `
+            <tr>
+                <td>${d.game}</td>
+                <td>${d.workShopItemIds.join(',')}</td>
+                <td>${d.numberOfSubscribedItems}</td>
+            </tr>
+        `;
+
+        html.push(temp);
+    });
+
+    document.getElementById('output').innerHTML = Array.isArray(html) ? html.join('') : html;
+}
+
+function getModData(path, workshopFiles) {
+    return new Promise( (resolve, reject) => {
+        const modData = [];
+
+        workshopFiles.forEach((f, i) => {
+            const filePath = `${path}\\workshop\\${f}`;
+            
+            fsExtra.readFile(filePath, "utf8", async (err, data) => {
+                if (err) {
+                    alert(err);
+                }
+                
+                const steamAppId = getSteamAppId(data);
+                const apiUrl = `https://store.steampowered.com/api/appdetails?appids=${steamAppId}`;
+                const result = await getData(apiUrl);
+
+                if (result[Object.keys(result)[0]].data) {
+                    const workShopItemIds = getWorkShopItemIds(data);
+                    modData.push({game: result[Object.keys(result)[0]]?.data?.name, workShopItemIds: workShopItemIds, numberOfSubscribedItems: workShopItemIds.length})
+                }
+                
+                if (workshopFiles.length === (i + 1)) {
+                    setTimeout(() => resolve(modData));
+                }
+            });
+        });
+    });
+}
+
+function getWorkShopItemIds(data) {
+    // Split .acf file find workshop ids
+    const wsMeta = data.split("WorkshopItemsInstalled")[1].split("WorkshopItemDetails")[0].split('').map((s) => s.trim()).filter((s) => s.length !== 0);
+    const parts = wsMeta.join('').split("{");
+    const ids = [];
+    const finalIds = [];
+
+    parts.forEach((s) => {
+        if (s.length > 1) {
+            const parts2 = s.split("}");
+            ids.push(parts2[parts2.length - 1]);
+        }
+    })
+
+    // Remove empty strings, remove quotes
+    ids.forEach((id) => {
+        const _id = id.split('"').filter((s) => s.length > 1)[0];
+
+        if (_id && _id.length > 1) {
+            finalIds.push(_id);
+        }
+    })
+
+    return finalIds;
+}
+
+function getSteamAppId(data) {
+    const trimed = data.split('"').map((s) => s.trim());
+
+    // Remove empty quotes
+    const trimed2 = trimed.filter((s) => s.length !== 0);
+    const appId = trimed2[trimed2.findIndex((s, i) => s === 'appid') + 1];
+
+    return appId;
+}
+
+function getData(url) {
+    return new Promise( (resolve, reject) => {
+        fetch(url).then(r => resolve(r.json()))
+    });
+}
+
+render();
+
